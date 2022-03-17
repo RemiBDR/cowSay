c4d9bd596570fc8a53184bb1980961144df9e81a
Mr BDR <mirebordier@gmail.com>
2022-03-17 22:43:31 +0100

    cowsay 2.0

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const userInfo = require("./information");
 console.log(`I'm ${userInfo.name} from ${userInfo.campus}.`);
 
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : `I'm ${userInfo.name} from ${userInfo.campus}.` ,
+    e : "oO",
+    T : "U "
+}));
